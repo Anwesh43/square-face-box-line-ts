fb48922688c524da9945bf1b59d5a81df3be2bd5
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 14:26:40 +0530

    addng SquareFaceBox class

--- index.ts
+++ index.ts
@@ -199,3 +199,26 @@ class SFBLNode {
         return this 
     }
 }
+
+class SquareFaceBox {
+
+    curr : SFBLNode = new SFBLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
