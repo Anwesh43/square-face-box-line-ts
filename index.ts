6b09499912c159370fba16334ceb87902d22118d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 14:17:39 +0530

    adding Stage class

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ const colors : Array<string> = [
     "#FFD600",
     "#01579B"
 ]
+const backColor : string = "#BDBDBD"
 
 class ScaleUtil {
 
@@ -80,5 +81,35 @@ class DrawingUtil {
         context.lineWidth = Math.min(w, h) / strokeFactor 
         context.strokeStyle = colors[i]
         DrawingUtil.drawSquareFaceBox(context, scale)
+    }
+}
+
+class Stage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D 
+
+    initCanvas() {
+        this.canvas.width = w 
+        this.canvas.height = h 
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : Stage = new Stage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
     }
 }
