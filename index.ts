1032681837eb9eac759b65a625afad3f4e685219
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 14:24:37 +0530

    adding SFBLNode class

--- index.ts
+++ index.ts
@@ -157,3 +157,45 @@ class Animator {
         }
     }
 }
+
+class SFBLNode {
+
+    next : SFBLNode 
+    prev : SFBLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SFBLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSFBLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SFBLNode {
+        var curr : SFBLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
