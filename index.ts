3d0f0a7c85d3db6b2251246549cd15e53e248663
Anwesh43 <anweshthecool0@gmail.com>
2021-06-05 14:11:28 +0530

    addng DrawingUtil class

--- index.ts
+++ index.ts
@@ -28,3 +28,57 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static translateDraw(context : CanvasRenderingContext2D, x : number, y : number, cb : Function) {
+        context.save()
+        context.translate(x, y)
+        cb(context)
+        context.restore()
+    }
+
+    static drawSquareFaceBox(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const eyeR : number = Math.min(w, h) / eyeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const color : string | CanvasGradient | CanvasPattern = context.fillStyle 
+        DrawingUtil.translateDraw(context, w / 2, h / 2, (ctx : CanvasRenderingContext2D) => {
+            DrawingUtil.translateDraw(ctx, -(w / 2 + size / 2) * (1 - sf1), 0, (ctx1 : CanvasRenderingContext2D) => {
+                ctx1.fillStyle = color 
+                ctx1.fillRect(-size / 2, -size / 2, size, size)
+            })
+            DrawingUtil.translateDraw(ctx, (w / 2 + size / 2) * (1 - sf2), size / 4, (ctx1 : CanvasRenderingContext2D) => {
+                DrawingUtil.drawLine(ctx1, -size / 2, 0, size / 2, 0)
+            })
+
+            for (var j = 0; j < 2 ; j++) {
+                DrawingUtil.drawCircle(ctx, (size / 4) * (1 - 2 * j) * sf4, -size / 4, eyeR * sf3)
+            }
+        })
+    }
+    
+    static drawSFBLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawSquareFaceBox(context, scale)
+    }
+}
